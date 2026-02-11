50a56e9f08fc3f8cb0f255d1bf6401431088c6a9
Isaac <garciai0526@hotmail.com>
2026-02-10 23:43:00 -0500

    first 2 assigments done

---
+++ Week4/KeywordTest1.java
@@ -0,0 +1,20 @@
+public class KeywordTest1 {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+        boolean flag = true;
+
+        if (number > 5) {
+            System.out.println("public static void inside string");
+        }
+
+        // public static if while (inside comment)
+
+        /*
+           class return new false
+        */
+
+        return;
+    }
+}
