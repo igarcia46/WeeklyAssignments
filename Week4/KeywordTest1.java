2a3479bd1cf48a27a6eda81fa5bdd2f0e7555c91
Isaac <garciai0526@hotmail.com>
2026-02-11 20:38:49 -0500

    exercise 24_03 done 1

--- Week4/KeywordTest1.java
+++ Week4/KeywordTest1.java
@@ -1,20 +1,15 @@
 public class KeywordTest1 {
-
     public static void main(String[] args) {
-
         int number = 10;
         boolean flag = true;
-
         if (number > 5) {
-            System.out.println("public static void inside string");
+            System.out.println("public static void but, its inside a string");
         }
 
         // public static if while (inside comment)
-
         /*
-           class return new false
+           class return new false in block comment
         */
-
         return;
     }
 }
